3f1e9e3d5ff9c43ecda3ef82da49d2b02290462a
manikandan <112394806+manikandan171@users.noreply.github.com>
2025-02-06 14:00:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@
     docker login -u mani1711 -p Rithvikmani123#
     docker tag test mani1711/t2
     docker push mani1711/t2
-    
+    kubectl create deployment manit3 --image=mani1711/t2 --port=80
+    kubectl expose deployment manit3 --type=NodePort --port=80
